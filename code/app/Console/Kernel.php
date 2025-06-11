7261d01bf3ce25dc65febe9f56a7f8a1926bc23f
ryan teixeira <ryantxr@gmail.com>
2025-06-11 17:05:38 -0400

    wip

--- code/app/Console/Kernel.php
+++ code/app/Console/Kernel.php
@@ -8,8 +8,6 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     protected $commands = [
-        \App\Console\Commands\GmailScan::class,
-        \App\Console\Commands\CreateUser::class,
     ];
 
     protected function schedule(Schedule $schedule): void
