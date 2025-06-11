6dd91067fc057fe1d771e150128cea1271977e01
ryan teixeira <ryantxr@gmail.com>
2025-06-11 18:25:35 -0400

    uplogo and dashboard

--- code/resources/js/components/AppLogoIcon.vue
+++ code/resources/js/components/AppLogoIcon.vue
@@ -13,12 +13,7 @@ defineProps<Props>();
 </script>
 
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 42" :class="className" v-bind="$attrs">
-        <path
-            fill="currentColor"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M17.2 5.633 8.6.855 0 5.633v26.51l16.2 9 16.2-9v-8.442l7.6-4.223V9.856l-8.6-4.777-8.6 4.777V18.3l-5.6 3.111V5.633ZM38 18.301l-5.6 3.11v-6.157l5.6-3.11V18.3Zm-1.06-7.856-5.54 3.078-5.54-3.079 5.54-3.078 5.54 3.079ZM24.8 18.3v-6.157l5.6 3.111v6.158L24.8 18.3Zm-1 1.732 5.54 3.078-13.14 7.302-5.54-3.078 13.14-7.3v-.002Zm-16.2 7.89 7.6 4.222V38.3L2 30.966V7.92l5.6 3.111v16.892ZM8.6 9.3 3.06 6.222 8.6 3.143l5.54 3.08L8.6 9.3Zm21.8 15.51-13.2 7.334V38.3l13.2-7.334v-6.156ZM9.6 11.034l5.6-3.11v14.6l-5.6 3.11v-14.6Z"
-        />
-    </svg>
+
+    <svg :class="className" v-bind="$attrs" width="100%" height="100%" viewBox="0 0 75 59" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;"><g><g><path d="M22.751,6.098l2.999,5.034" style="fill:none;stroke:#000;stroke-width:2.89px;"/><path d="M33.878,3.387l-0,7.197" style="fill:none;stroke:#000;stroke-width:2.89px;"/><path d="M45.098,5.931l-3.572,5.432" style="fill:none;stroke:#000;stroke-width:2.89px;"/></g><g><path d="M59.601,18.713c-0.112,-1.783 -1.651,-3.14 -3.435,-3.027l-49.207,3.102c-1.783,0.113 -3.14,1.652 -3.027,3.435l1.895,30.064c0.113,1.783 1.652,3.139 3.435,3.027l49.208,-3.103c1.783,-0.112 3.139,-1.651 3.027,-3.435l-1.896,-30.063Z" style="fill:#f9c85e;stroke:#e94900;stroke-width:2.89px;"/><path d="M20.535,33.449l-14.926,-13.83" style="fill:none;stroke:#e94900;stroke-width:2.89px;"/><path d="M21.357,39.168l-14.234,14.899" style="fill:none;stroke:#e94900;stroke-width:2.89px;"/><path d="M45.202,32.018l13.283,-15.457" style="fill:none;stroke:#e94900;stroke-width:2.89px;"/></g><g><path d="M24.87,24.248c1.778,-0.223 3.484,1.695 3.807,4.278c0.324,2.584 -0.858,4.863 -2.636,5.085c-1.778,0.223 -3.484,-1.695 -3.807,-4.278c-0.324,-2.584 0.858,-4.863 2.636,-5.085Z"/><ellipse cx="26.215" cy="28.217" rx="0.74" ry="1.032" style="fill:#ebebeb;"/></g><g><ellipse cx="40.131" cy="28.006" rx="3.138" ry="4.291"/><ellipse cx="40.727" cy="27.164" rx="0.698" ry="0.809" style="fill:#dadada;"/></g><path d="M27.715,37.541c1.696,3.898 10.464,5.082 12.367,-1.255" style="fill:none;stroke:#000;stroke-width:3.05px;"/><path d="M52.702,44.609l-4.102,-5.176c-2.542,-2.977 -7.345,-0.027 -4.891,4.207l7.198,9.24c1.719,1.665 3.769,1.357 5.761,-0.895l13.776,-17.588c1.004,-1.508 0.799,-4.044 -0.541,-4.865c-1.289,-0.79 -2.627,-0.764 -4.022,0.319l-13.179,14.758Z" style="fill:#4d9a5b;stroke:#000;stroke-width:2.89px;"/></g></svg>
+
 </template>
