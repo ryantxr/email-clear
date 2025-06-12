6e512e62b533d8a3aa9654cec7eab430c1452d0d
ryan teixeira <ryantxr@gmail.com>
2025-06-11 20:05:48 -0400

    save settings

--- code/app/Http/Controllers/Admin/SiteContentController.php
+++ code/app/Http/Controllers/Admin/SiteContentController.php
@@ -7,6 +7,7 @@ use App\Models\Setting;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
+use Illuminate\Support\Facades\Log;
 
 class SiteContentController extends Controller
 {
@@ -20,12 +21,14 @@ class SiteContentController extends Controller
 
     public function update(Request $request)
     {
+        Log::info(__METHOD__);
         $data = $request->validate([
             'front_page_text' => ['nullable', 'string'],
             'pricing' => ['nullable', 'string'],
         ]);
-
+        Log::debug("validated");
         foreach ($data as $key => $value) {
+            Log::debug("Update $key");
             Setting::updateOrCreate(['key' => $key], ['value' => $value]);
         }
 
