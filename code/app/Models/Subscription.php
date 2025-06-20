225434d48a6959fe77a07e13e902cff9187868f0
Ryan Teixeira <ryantxr@gmail.com>
2025-06-20 01:17:58 -0400

    feat: allow upgrading free plan to pro

--- code/app/Models/Subscription.php
+++ code/app/Models/Subscription.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subscription extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'plan_id',
+        'price',
+        'start_at',
+        'last_charged_at',
+        'paused_at',
+        'end_at',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function plan()
+    {
+        return $this->belongsTo(Plan::class);
+    }
 }
